7346433ef3823e99b52b0bcff0f55385385768b9
gaforb <38703458+gaforb@users.noreply.github.com>
2024-09-20 08:04:46 +0100

    Add files via upload
    I summon almighty Borbo to fix this godawful nonsense

---
+++ MageEruption.cs
@@ -0,0 +1,119 @@
+using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using RoR2.Skills;
+using EntityStates;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace MageEruption
+{
+    [BepInDependency(R2API.ContentManagement.R2APIContentManager.PluginGUID)]
+    [BepInDependency(LanguageAPI.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class MageEruption : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "gaforb";
+        public const string PluginName = "MageEruption";
+        public const string PluginVersion = "3.2.0";
+
+        public static ConfigEntry<bool> VFXoff { get; set; }
+
+        public static ConfigEntry<bool> IsSprint { get; set; }
+        public static ConfigEntry<bool> IsSpecial { get; set; }
+
+        public static ConfigEntry<float> BlastDmg { get; set; }
+        public static ConfigEntry<float> BlastArea { get; set; }
+        public static ConfigEntry<float> BlastProc { get; set; }
+        public static ConfigEntry<float> BlastForce { get; set; }
+        public static ConfigEntry<float> Cooldown { get; set; }
+        public static ConfigEntry<int> Stocks { get; set; }
+
+        public void Awake()
+        {
+            MageEruption.VFXoff = base.Config.Bind<bool>("General", "Use simple explosion", false, "Disables some graphical effects, improving performance.");
+            MageEruption.IsSprint = base.Config.Bind<bool>("General", "Force sprinting", false, "Makes Eruption work like other utility skills and force sprint. if True, using Flamethrower during Eruption will waste the Flamethrower stock.");
+            MageEruption.IsSpecial = base.Config.Bind<bool>("General", "Move to special", false, "Adds Eruption to the Special slot instead of the Utility slot.");
+            MageEruption.BlastDmg = base.Config.Bind<float>("Values", "Blast damage", 3.6f, "How much damage the initial blast deals. 1 = 100%. Setting to 0 will still apply burn.");
+            MageEruption.BlastArea = base.Config.Bind<float>("Values", "Blast area", 8f, "How much range the intial blast has. Set to 0 to disable blast attack completely");
+            MageEruption.BlastProc = base.Config.Bind<float>("Values", "Blast proc coefficient", 1f, "How well Eruption triggers item effects. Also effects burn duration.");
+            MageEruption.BlastForce = base.Config.Bind<float>("Values", "Blast force", 12f, "How much Eruption knocks back enemies.");
+            MageEruption.Cooldown = base.Config.Bind<float>("Values", "Cooldown", 7f, "How long Eruption takes to recharge.");
+            MageEruption.Stocks = base.Config.Bind<int>("Values", "Stocks", 1, "Number of stocks Eruption has.");
+
+            LanguageAPI.Add("MAGE_ERUPTION_FIRE", "Eruption");
+            LanguageAPI.Add("MAGE_ERUPTION_DESCRIPTION", "Blast forward" + ((MageEruption.BlastArea.Value > 0f) ? (", " + ((MageEruption.BlastDmg.Value > 0f) ? string.Format("dealing <style=cIsDamage>{0}%</style> damage and ", MageEruption.BlastDmg.Value * 100f) : "") + "<style=cIsDamage>igniting</style> nearby enemies") : "") + "." + ((MageEruption.Stocks.Value != 1) ? string.Format(" Holds up to {0} charges.", MageEruption.Stocks.Value) : ""));
+
+            SkillDef skillDef = ScriptableObject.CreateInstance<SkillDef>();
+            skillDef.activationState = new SerializableEntityStateType(typeof(MageEruptionState));
+            skillDef.activationStateMachineName = "Body";
+            skillDef.baseMaxStock = Mathf.Max(MageEruption.Stocks.Value, 1);
+            skillDef.baseRechargeInterval = Mathf.Max(MageEruption.Cooldown.Value, 0.5f);
+            skillDef.beginSkillCooldownOnSkillEnd = false;
+            skillDef.canceledFromSprinting = false;
+            skillDef.forceSprintDuringState = MageEruption.IsSprint.Value;
+            skillDef.cancelSprintingOnActivation = !MageEruption.IsSprint.Value;
+            skillDef.isCombatSkill = (MageEruption.BlastArea.Value > 0f);
+            skillDef.fullRestockOnAssign = true;
+            skillDef.interruptPriority = InterruptPriority.PrioritySkill;
+            skillDef.mustKeyPress = true;
+            skillDef.rechargeStock = 1;
+            skillDef.requiredStock = 1;
+            skillDef.stockToConsume = 1;
+            skillDef.icon = MageEruption.SpriteFromFile("eruption.png");
+            skillDef.skillName = "MAGE_ERUPTION_FIRE";
+            skillDef.skillNameToken = "MAGE_ERUPTION_FIRE";
+            skillDef.skillDescriptionToken = "MAGE_ERUPTION_DESCRIPTION";
+            GameObject mageBodyprefab = Addressables.LoadAssetAsync<GameObject>("mage").WaitForCompletion();
+            bool wasAdded;
+            ContentAddition.AddEntityState(typeof(MageEruptionState), out wasAdded);
+            ContentAddition.AddSkillDef(skillDef);
+
+            //idk why I can't put the if statement up here instead but whatever this works
+            SkillFamily skillFamily = Addressables.LoadAssetAsync<SkillFamily>("RoR2/Base/Mage/MageBodySpecialFamily.asset").WaitForCompletion();
+            SkillFamily skillFamily2 = Addressables.LoadAssetAsync<SkillFamily>("RoR2/Base/Mage/MageBodyUtilityFamily.asset").WaitForCompletion();
+            
+            if (IsSpecial.Value)
+            {
+                Array.Resize<SkillFamily.Variant>(ref skillFamily.variants, skillFamily.variants.Length + 1);
+                SkillFamily.Variant[] variants = skillFamily.variants;
+                int num = skillFamily.variants.Length - 1;
+                SkillFamily.Variant variant = default(SkillFamily.Variant);
+                variant.skillDef = skillDef;
+                variant.unlockableDef = null;
+                variant.viewableNode = new ViewablesCatalog.Node(skillDef.skillNameToken, false, null);
+                variants[num] = variant;
+            }
+            else
+            {
+                Array.Resize<SkillFamily.Variant>(ref skillFamily2.variants, skillFamily2.variants.Length + 1);
+                SkillFamily.Variant[] variants2 = skillFamily2.variants;
+                int num2 = skillFamily2.variants.Length - 1;
+                SkillFamily.Variant variant = default(SkillFamily.Variant);
+                variant.skillDef = skillDef;
+                variant.unlockableDef = null;
+                variant.viewableNode = new ViewablesCatalog.Node(skillDef.skillNameToken, false, null);
+                variants2[num2] = variant;
+            }
+        }
+        public static Sprite SpriteFromFile(string name)
+        {
+            Texture2D texture2D = new Texture2D(2, 2);
+            try
+            {
+                ImageConversion.LoadImage(texture2D, File.ReadAllBytes(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), name)));
+            }
+            catch (FileNotFoundException ex)
+            {
+                Debug.LogError("Failed to read file at " + ex.FileName);
+                return null;
+            }
+            return Sprite.Create(texture2D, new Rect(0f, 0f, (float)texture2D.height, (float)texture2D.width), new Vector2(0.5f, 0.5f), 100f);
+        }
+    }
+}
---
+++ MageEruptionState.cs
@@ -0,0 +1,119 @@
+using EntityStates;
+using RoR2;
+using UnityEngine;
+using EntityStates.LemurianBruiserMonster;
+using EntityStates.Mage;
+using UnityEngine.UIElements;
+using UnityEngine.Networking;
+
+
+namespace MageEruption
+{
+    public class MageEruptionState : GenericCharacterMain
+    {
+        private Vector3 flyVector = Vector3.zero;
+        private Vector3 blastPosition;
+        public float baseDuration = 1.2f;
+        private float aoe = Mathf.Max(MageEruption.BlastArea.Value, 0f);
+        private float dmg = Mathf.Max(MageEruption.BlastDmg.Value, 0f);
+        private float proc = Mathf.Max(MageEruption.BlastProc.Value, 0f);
+        private float force = Mathf.Max(MageEruption.BlastForce.Value, 0f) * 100f;
+        private bool vfxoff = MageEruption.VFXoff.Value;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Debug.Log("base done");
+            float angleV = base.isGrounded ? 1.1f : 3.3f;
+            this.flyVector = Vector3.Normalize(base.characterDirection.forward + Vector3.up / angleV);
+            Debug.Log(this.flyVector);
+            Util.PlaySound(FlyUpState.beginSoundString, base.gameObject);
+            Debug.Log(FlyUpState.beginSoundString);
+            this.CreateBlinkEffect(base.characterBody.corePosition);
+            base.PlayCrossfade("Body", "FlyUp", "FlyUp.playbackRate", this.baseDuration, 0.1f);
+            base.characterMotor.Motor.ForceUnground();
+            base.characterMotor.velocity = Vector3.zero;
+            this.blastPosition = base.characterBody.corePosition;
+            bool hasAOE = base.isAuthority && this.aoe > 0f;
+            if (hasAOE)
+            {
+                BlastAttack blastAttack = new BlastAttack
+                {
+                    radius = this.aoe,
+                    procCoefficient = this.proc,
+                    position = this.blastPosition,
+                    attacker = base.gameObject,
+                    crit = Util.CheckRoll(base.characterBody.crit, base.characterBody.master),
+                    baseDamage = base.characterBody.damage * this.dmg,
+                    falloffModel = 0,
+                    baseForce = this.force,
+                    attackerFiltering = AttackerFiltering.NeverHitSelf,
+                    damageType = DamageType.IgniteOnHit
+                };
+                blastAttack.teamIndex = TeamComponent.GetObjectTeam(blastAttack.attacker);
+                blastAttack.Fire();
+            }
+        }
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            bool timeUp = base.fixedAge >= this.baseDuration -0.4 && base.isAuthority;
+            if (timeUp)
+            {
+                this.outer.SetNextStateToMain();
+            }
+        }
+        public override void OnExit()
+        {
+            bool done = !this.outer.destroying;
+            if (done)
+            {
+                Util.PlaySound(FlyUpState.endSoundString, base.gameObject);
+            }
+            base.OnExit();
+        }
+        public override void OnSerialize(NetworkWriter writer)
+        {
+            base.OnSerialize(writer);
+            writer.Write(this.blastPosition);
+        }
+        public override void OnDeserialize(NetworkReader reader)
+        {
+            base.OnDeserialize(reader);
+            this.blastPosition = reader.ReadVector3();
+        }
+        public override void HandleMovements()
+        {
+            base.HandleMovements();
+            float finalSpeed = 2f + this.moveSpeedStat * (base.characterBody.isSprinting ? (1f / base.characterBody.sprintingSpeedMultiplier) : 1f) / 2f * FlyUpState.speedCoefficientCurve.Evaluate(base.fixedAge / this.baseDuration);
+            base.characterMotor.rootMotion += this.flyVector * finalSpeed * Time.fixedDeltaTime;
+            CharacterMotor characterMotor = base.characterMotor;
+            characterMotor.velocity.y = characterMotor.velocity.y + 0.3f;
+        }
+        //public override void ProcessJump()
+        //{
+        //    base.ProcessJump();
+        //    this.jetpackStateMachine.SetNextState(new Idle());
+        //}
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.PrioritySkill;
+        }
+        private void CreateBlinkEffect(Vector3 origin)
+        {
+            if (!vfxoff)
+            {
+                EffectData effectData = new EffectData
+                {
+                    rotation = Util.QuaternionSafeLookRotation(this.flyVector),
+                    origin = origin,
+                    scale = this.aoe
+                };
+                EffectManager.SpawnEffect(Resources.Load<GameObject>("Prefabs/Effects/ImpactEffects/ExplosionSolarFlare"), effectData, false);
+                EffectManager.SpawnEffect(Resources.Load<GameObject>("Prefabs/Effects/ImpactEffects/MagmaOrbExplosion"), effectData, false);
+            }
+            EffectManager.SimpleMuzzleFlash(FireMegaFireball.muzzleflashEffectPrefab, base.gameObject, "MuzzleLeft", false);
+            EffectManager.SimpleMuzzleFlash(FireMegaFireball.muzzleflashEffectPrefab, base.gameObject, "MuzzleRight", false);
+        }
+    }
+}
